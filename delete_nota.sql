fb7c5015a0046ca4558f2369e0226999d23ad6c0
sebasrojasl <42528969+sebasrojasl@users.noreply.github.com>
2019-04-21 19:01:50 -0600

    Create delete_nota.sql

---
+++ delete_nota.sql
@@ -0,0 +1,23 @@
+create or replace PROCEDURE delete_nota
+(vcid_nota in nota.id_nota%type) AS
+--- Procedimiento que ingresa al catalogo un tipo de delito  nuevo
+BEGIN
+    SAVEPOINT sp_safe;
+        DELETE
+        FROM
+            nota
+        WHERE
+            id_nota = vcid_nota;
+   commit;
+   EXCEPTION 
+WHEN STORAGE_ERROR THEN 
+       DBMS_OUTPUT.PUT_LINE('An error was encountered - '||SQLCODE||' -ERROR- '||SQLERRM);
+    ROLLBACK to sp_safe;
+WHEN SUBSCRIPT_BEYOND_COUNT THEN
+       DBMS_OUTPUT.PUT_LINE('An error was encountered - '||SQLCODE||' -ERROR- '||SQLERRM);
+    ROLLBACK to sp_safe;
+WHEN OTHERS THEN 
+        DBMS_OUTPUT.PUT_LINE('An error was encountered - '||SQLCODE||' -ERROR- '||SQLERRM);
+    ROLLBACK to sp_safe; 
+     --maneja cualquier otro error      
+END;
