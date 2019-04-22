87f8f6f74892e8805207b2535e481e6e3d238d40
sebasrojasl <42528969+sebasrojasl@users.noreply.github.com>
2019-04-21 19:04:24 -0600

    Create delete_genero.sql

---
+++ delete_genero.sql
@@ -0,0 +1,22 @@
+create or replace procedure deleteGenero(vcNombre in Genero.nombre%type)is 
+    BEGIN
+        savepoint sp_safe;
+        --Punto de seguridad por si pasa un error
+        delete
+        from Genero
+        where nombre = vcNombre;
+        COMMIT;
+    EXCEPTION
+    --En caso de algún error, aquí se hace un catch del mismo y se envía al dbms_output para desde ahí leerlo en Java
+       WHEN NO_DATA_FOUND THEN 
+            DBMS_OUTPUT.PUT_LINE('An error was encountered - '||SQLCODE||' -ERROR- '||SQLERRM);
+            ROLLBACK to sp_safe;
+        WHEN STORAGE_ERROR THEN
+            DBMS_OUTPUT.PUT_LINE('An error was encountered - '||SQLCODE||' -ERROR- '||SQLERRM);
+            ROLLBACK to sp_safe;
+        WHEN OTHERS THEN
+            dbms_output.put_line('An error was encountered - '||SQLCODE||' -ERROR- '||SQLERRM);
+            ROLLBACK to sp_safe;     
+             --maneja cualquier otro error 
+        COMMIT;
+    END;
