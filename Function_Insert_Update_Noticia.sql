7cc513226a371ffb7c5e1ea7f3c39a953eeada6e
estebgonza29 <48929312+estebgonza29@users.noreply.github.com>
2019-04-21 20:26:38 -0600

    Funcion Insertar, Actualizar y Borrar Noticia

--- Function_Insert_Update_Noticia.sql
+++ Function_Insert_Update_Noticia.sql
@@ -52,3 +52,27 @@ vcComunidad in noticia.id_comunidad%type, vcClasif_Noticia in noticia.id_clasif_
              --maneja cualquier otro error 
         COMMIT;
     END;
+    
+--Procedimiento que se encarga de eliminar cierto dato de la tabla Noticia dependiendo del id_Noticia ingresado
+create or replace procedure deleteNoticia(vcId_Noticia in Noticia.id_noticia%type)is 
+    BEGIN
+        savepoint sp_safe;
+        --Punto de seguridad por si pasa un error
+        delete
+        from Noticia
+        where id_noticia = vcId_Noticia;
+        COMMIT;
+    EXCEPTION
+    --En caso de algún error, aquí se hace un catch del mismo y se envía al dbms_output para desde ahí leerlo en Java
+       WHEN NO_DATA_FOUND THEN 
+            DBMS_OUTPUT.PUT_LINE('An error was encountered - '||SQLCODE||' -ERROR- '||SQLERRM);
+            ROLLBACK to sp_safe;
+        WHEN STORAGE_ERROR THEN
+            DBMS_OUTPUT.PUT_LINE('An error was encountered - '||SQLCODE||' -ERROR- '||SQLERRM);
+            ROLLBACK to sp_safe;
+        WHEN OTHERS THEN
+            dbms_output.put_line('An error was encountered - '||SQLCODE||' -ERROR- '||SQLERRM);
+            ROLLBACK to sp_safe;     
+             --maneja cualquier otro error 
+        COMMIT;
+    END;
