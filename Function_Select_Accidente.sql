3f53a5683ed513093696e761f9cb29bc360dd507
estebgonza29 <48929312+estebgonza29@users.noreply.github.com>
2019-04-21 20:32:59 -0600

    Funcion getAccidente

---
+++ Function_Select_Accidente.sql
@@ -0,0 +1,27 @@
+--Funcion que se encarga de obtener todos los datos de un Accidente de Transito dependiendo de un id_usuario ingresado
+-- Se introducen los datos en un cursor y se retornan dentro del mismo
+CREATE OR REPLACE FUNCTION getAccidenteTransito(pId_Usuario in Number) RETURN SYS_REFCURSOR
+AS
+    curAccidenteTrans SYS_REFCURSOR;
+    BEGIN
+      SAVEPOINT sp_safe;
+      --Punto de seguridad por si pasa un error
+      OPEN curAccidenteTrans
+      FOR SELECT * 
+      FROM Accidente_Transito
+      WHERE id_Usuario = pId_Usuario;
+      RETURN curAccidenteTrans;
+      Close curAccidenteTrans;
+  EXCEPTION
+  --En caso de algún error, aquí se hace un catch del mismo y se envía al dbms_output para desde ahí leerlo en Java
+       WHEN NO_DATA_FOUND THEN 
+            DBMS_OUTPUT.PUT_LINE('An error was encountered - '||SQLCODE||' -ERROR- '||SQLERRM);
+            ROLLBACK to sp_safe;
+        WHEN STORAGE_ERROR THEN
+            DBMS_OUTPUT.PUT_LINE('An error was encountered - '||SQLCODE||' -ERROR- '||SQLERRM);
+            ROLLBACK to sp_safe;
+        WHEN OTHERS THEN
+            dbms_output.put_line('An error was encountered - '||SQLCODE||' -ERROR- '||SQLERRM);
+            ROLLBACK to sp_safe;     
+             --maneja cualquier otro error 
+END getAccidenteTransito;
